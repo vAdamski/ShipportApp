71372ecbba25217a51ce9d7c687b8cb34163ffe1
Adam Ludwiczak <adam.ludwiczak98@gmail.com>
2021-10-24 11:29:25 +0200

    Update query and added new controller

---
+++ ShipportApp.Application/Rotations/Queries/GetRotation/TerminalDto.cs
@@ -0,0 +1,19 @@
+﻿using ShipportApp.Application.Cargoes.Queries.GetCargoes;
+using ShipportApp.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipportApp.Application.Terminals.Queries.GetTerminals
+{
+    public class TerminalDto
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public List<Cargo> Cargoes { get; set; }
+        public DateTime? ATB { get; set; }
+        public DateTime? ATD { get; set; }
+    }
+}
---
+++ ShipportApp/Controllers/RotationController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ShipportApp.Application.Cargoes.Commands.CreateCargo;
+using ShipportApp.Application.Cargoes.Commands.DeleteCargo;
+using ShipportApp.Application.Cargoes.Queries.GetCargoes;
+using ShipportApp.Domain.Entities;
+using System.Threading.Tasks;
+
+namespace ShipportApp.Controllers
+{
+    [Route("api/cargoes")]
+    public class CargoesController : BaseController
+    {
+        [HttpGet]
+        public async Task<ActionResult<CargoesVm>> GetAsync()
+        {
+            var cargoesVm = await Mediator.Send(new GetCargoesQuery { });
+
+            if (cargoesVm == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cargoesVm);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<string>> PostAsync([FromBody] Cargo cargo)
+        {
+            var id = await Mediator.Send(
+                new CreateCargoCommand()
+                {
+                    Name = cargo.Name,
+                    TerminalId = cargo.TerminalId,
+                    ATC = cargo.ATC
+                });
+
+            return Ok(id);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> DeleteCargo(string id)
+        {
+            if (id == null)
+            {
+                return BadRequest(id);
+            }
+
+            await Mediator.Send(new DeleteCargoCommand() { CargoId = id });
+
+            return Ok();
+        }
+    }
+}
